877ed7592371a8e888eee563f9faafc3e133dffc
sam156 <bskordo@gmail.com>
2016-10-10 01:20:06 -0400

    переписан код согласно перепроверки

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # 5_lang_frequency
+
+
+Library:
+1.import os for operating system dependent functionality
+2.import re to provides regular expression matching operations
+3.import collections to implements specialized container datatypes
+4.from collections import Counter for a dict subclass for counting hashable objects
+
+Functions:
+1.def load_data(filepath)--This functin will upload a text in which should count a words
+2.def get_most_frequent_words()---Will find the most frequently words also it will
+provide the path where to need upload data ,user should define the path in load_data 
+where exist a data.
+3.if __name__ == '__main__'--will show the result
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,15 +1,25 @@
-
+import os
 import re
+import collections
 from collections import Counter
 
-with open('myfile.txt') as f:
-    passage = f.read()
+def load_data(filepath):
+    if not os.path.exists(filepath):
+       return None
+    with open(filepath, 'r',encoding = 'utf-8') as file_handler:
+       data = file_handler.read()
+       return (data)
 
-words = re.findall(r'\w+', passage)
+def get_most_frequent_words():
+    action =  load_data(r'filepath')
+    words = re.findall(r'\w+', action)
+    resulting_count = collections.Counter(words)
+    word_counts = Counter(resulting_count)
+    top_ten = word_counts.most_common(10)
+    return (top_ten)
 
-cap_words = [word.upper() for word in words]
 
-word_counts = Counter(cap_words)
-top_ten = word_counts.most_common(10)
-print(top_ten)
 
+if __name__ == '__main__':
+    result = get_most_frequent_words()
+    print (result)
